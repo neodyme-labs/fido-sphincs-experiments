522d2f1f27a1214adb910d92da22a37d69eda26a
Ruben Gonzalez <ruben@neodyme.io>
2025-02-20 10:15:40 +0100

    Update plot script to eprint version

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,7 +7,6 @@ def extract_data(data):
     siglen = []
     nhashes = []
     print(data)
-    print("huso")
     for d in data.split("\n"):
         if not d:
             continue
@@ -30,21 +29,30 @@ num_hashes_256, signature_size_256 = extract_data(open(argv[3]).read())
 # num_hashes_x, signature_size_x = extract_data(open(argv[4]).read())
 
 # Create scatter plot
-plt.figure(figsize=(8, 6))
-plt.scatter(num_hashes_128, signature_size_128, color='blue', marker='x', label='$q = 2^8$')
-plt.scatter(num_hashes_192, signature_size_192, color='green', marker='s', label='$q = 2^{10}$')
-plt.scatter(num_hashes_256, signature_size_256, color='red', marker='d', label='$q = 2^{16}$')
+fig = plt.figure(figsize=(8, 6))
+plt.scatter(num_hashes_192, signature_size_192, color='orange', marker='s', label='$q = 2^{10}$')
+plt.scatter(num_hashes_128, signature_size_128, color='blue', marker='+', label='$q = 2^8$')
+plt.scatter(num_hashes_256, signature_size_256, color='red', marker='o', label='$q = 2^{16}$')
 # plt.scatter(num_hashes_x, signature_size_x, color='red', marker='d', label='x-bit')
 plt.axhline(y=15161, color='black', linestyle='-', linewidth=1)
-plt.text(400, 15161, "Aligned CTAP USB HID Message Limit", verticalalignment='bottom', fontsize=14, color='black')
+plt.text(2000, 15161, "Aligned CTAP USB HID Message Limit", verticalalignment='bottom', fontsize=14, color='black')
 # Labels and title with larger font size
-plt.xlabel("Hash Computations ($10^3$)", fontsize=16)
-plt.ylabel("Signature size (bytes)", fontsize=16)
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.legend(fontsize=16, loc='upper left', frameon=True, edgecolor='black', bbox_to_anchor=(1.05, 1))
-plt.grid(True, linestyle='--', alpha=0.6)
-
+plt.xlabel("Hash Computations ($10^3$)", fontsize=16, fontname="serif")
+plt.ylabel("Signature size (bytes)", fontsize=16, fontname="serif")
+plt.xticks(fontsize=14, fontname="serif")
+plt.yticks(fontsize=14, fontname="serif")
+handles, labels = plt.gca().get_legend_handles_labels()
+order = [1,0,2]
+plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order], fontsize=16, loc='upper center', frameon=False, bbox_to_anchor=(0.5, 1.15), ncol=3)
+plt.grid(True, linestyle='--', axis="y", linewidth=0.5, alpha=0.6)
+fig.set_edgecolor("gray")
+ax = plt.gca()
+for spine in ["left", "bottom"]:
+    ax.spines[spine].set_edgecolor("gray")
+for spine in ["right", "top"]:
+    ax.spines[spine].set_visible(False)
+ax.set_ylim(13000, None)
+ax.set_xlim(0, None)
 # Save the figure for a scientific paper
 plt.savefig("scatter_plot.pdf", dpi=900, bbox_inches='tight')
 
